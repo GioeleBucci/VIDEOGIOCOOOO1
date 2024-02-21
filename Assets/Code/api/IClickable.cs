4590bde1c6463f165828242fd15fa073cb62bd53
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-02-21 22:56:00 +0100

    refactor: changed IClickable so as to use library functions

--- Assets/Code/api/IClickable.cs
+++ Assets/Code/api/IClickable.cs
@@ -1,8 +1,10 @@
+using UnityEngine.EventSystems;
+
 /*
  An interface modelling an object that can be clicked. Players and suitable
 terrain tiles should implement this interface.
  */
-public interface IClickable
+public interface IClickable : IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerExitHandler
 {
     void OnClick();
 }
