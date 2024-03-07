83c8d8a497d43b4ad12bd8e1f2b2d10732407cc1
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-03-07 17:31:56 +0100

    docs: written documentation for IClickable

--- Assets/Code/api/IClickable.cs
+++ Assets/Code/api/IClickable.cs
@@ -7,10 +7,18 @@ terrain tiles should implement this interface.
  */
 public interface IClickable
 {
+    /// <summary>
+    /// Get the <see cref="Transform"/> of this clickable entity.
+    /// </summary>
+    /// <returns>The <see cref="Transform"/> of this IClickable.</returns>
     Transform GetTransform();
-
+    /// <summary>
+    /// Entity behaviour on selection, called by the <see cref="ClickManager"/>.
+    /// </summary>
     void OnSelection();
+    /// <summary>
+    /// Entity behaviour on deselection, called by the <see cref="ClickManager"/>.
+    /// </summary>
     void OnDeselection();
-
 }
 
