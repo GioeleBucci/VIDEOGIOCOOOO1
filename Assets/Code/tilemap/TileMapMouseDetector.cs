3ec203f48f45fe49fd7bd8c99f1e029f00060d18
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-03-21 10:10:31 +0100

    feat: added tile coordinates detector

--- Assets/Code/tilemap/TileMapMouseDetector.cs
+++ Assets/Code/tilemap/TileMapMouseDetector.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+/// <summary>
+/// This script may be attached to a tilemap to detect the tile coordinates based on the position
+/// of the mouse cursor.
+/// </summary>
 public class TileMapMouseDetector : MonoBehaviour
 {
     [SerializeField]
     private Tilemap tilemap;
+    private GridLayout gridLayout;
+    private Vector3Int tilePosition;
 
-    //TODO
     void Start()
     {
-        
+        this.gridLayout = tilemap.GetComponentInParent<GridLayout>();
+        Debug.Log("I want auto-scrolling!!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("Mouse position: " +  mousePos);
+        tilePosition = gridLayout.WorldToCell(mousePos);
+        Debug.Log("Tile coordinates: " + tilePosition);
     }
 }
